581fae3e7d0b6184bd99cb9540be99ea47189423
jessica <jessica_rolland@outlook.fr>
2018-07-13 11:28:41 +0200

    entity etape

---
+++ projet_coiffeur/src/Entity/Etape.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EtapeRepository")
+ */
+class Etape
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $duree;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $ressource;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDuree(): ?int
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(int $duree): self
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getRessource(): ?bool
+    {
+        return $this->ressource;
+    }
+
+    public function setRessource(bool $ressource): self
+    {
+        $this->ressource = $ressource;
+
+        return $this;
+    }
+}
